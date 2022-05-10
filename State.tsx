e948abc87a03a9b8aa602111ba81cb38481759d9
Dario Castro-Lopez <70664410+LonnieC11@users.noreply.github.com>
2022-05-10 10:49:19 -0700

    Add files via upload

---
+++ State.tsx
@@ -0,0 +1,38 @@
+// 3rd party
+import { List, Map } from 'immutable';
+
+// project dependencies
+import { PianoInstrument } from './instruments/Piano';
+import { WaveformVisualizer } from './visualizers/Waveform';
+import { LonnieC11 } from './instruments/dcastrolopez';
+import { dcastrolopezVisualizer } from './visualizers/dcastrolopezVisualizer';
+
+
+/** ------------------------------------------------------------------------ **
+ * The entire application state is stored in AppState.
+ ** ------------------------------------------------------------------------ */
+export type AppState = Map<string, any>;           // similar to { [id: string]: any }
+
+/**
+ * Start with the default piano instrument.
+ * Add your instruments to this list.
+ */
+const instruments = List([PianoInstrument, LonnieC11]);       // similar to Instrument[]
+
+/**
+ * Start with the default waveform visualizer.
+ * Add your visualizers to this list.
+ */
+const visualizers = List([WaveformVisualizer,dcastrolopezVisualizer]);    // similar to Visualizer[]
+
+
+/**
+ * The default application state contains a list of instruments and a list of visualizers.
+ *
+ * 'instrument': List<Instrument>
+ * 'visualizer': List<Visualizer>
+ */
+export const defaultState: AppState = Map<string, any>({
+  'instruments': instruments,
+  'visualizers': visualizers,
+});
