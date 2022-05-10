4b6b79f308ce3cb4f0927a0db8a2f2a471a970dd
Dario Castro-Lopez <70664410+LonnieC11@users.noreply.github.com>
2022-05-10 10:52:57 -0700

    Add files via upload

--- client/src/State.tsx
+++ client/src/State.tsx
@@ -4,7 +4,8 @@ import { List, Map } from 'immutable';
 // project dependencies
 import { PianoInstrument } from './instruments/Piano';
 import { WaveformVisualizer } from './visualizers/Waveform';
-import { dcastrolopez } from './instruments/dcastrolopez';
+import { LonnieC11 } from './instruments/dcastrolopez';
+import { dcastrolopezVisualizer } from './visualizers/dcastrolopezVisualizer';
 
 
 /** ------------------------------------------------------------------------ **
@@ -16,13 +17,13 @@ export type AppState = Map<string, any>;           // similar to { [id: string]:
  * Start with the default piano instrument.
  * Add your instruments to this list.
  */
-const instruments = List([PianoInstrument, dcastrolopez]);       // similar to Instrument[]
+const instruments = List([PianoInstrument, LonnieC11]);       // similar to Instrument[]
 
 /**
  * Start with the default waveform visualizer.
  * Add your visualizers to this list.
  */
-const visualizers = List([WaveformVisualizer]);    // similar to Visualizer[]
+const visualizers = List([WaveformVisualizer,dcastrolopezVisualizer]);    // similar to Visualizer[]
 
 
 /**
